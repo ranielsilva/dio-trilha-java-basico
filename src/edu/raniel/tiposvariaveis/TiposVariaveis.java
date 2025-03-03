5c370d281b28052d565f678edc0751794d332ece
raniel silva <ranielcorreia1997@gmail.com>
2025-03-03 15:07:48 -0300

    adicionando escorpo do código com os tipos de variáveis

--- src/edu/raniel/tiposvariaveis/TiposVariaveis.java
+++ src/edu/raniel/tiposvariaveis/TiposVariaveis.java
@@ -1,4 +1,28 @@
 package edu.raniel.tiposvariaveis;
 
 public class TiposVariaveis {
+    // Tipo inteiro (int)
+    int numero = 30;
+
+    // Tipo ponto flutuante (double)
+    double altura = 1.75;
+
+    //ponto flutuante do tipo float
+    float peso = 64.5f;
+
+    //Tipo de numero longo
+    long numeroLongo = 879845645123168784l;
+
+    // Tipo caractere (char)
+    char letra = 'A';
+
+    // Tipo booleano (boolean)
+    boolean valor = true;
+
+    // Tipo String (sequência de caracteres)
+    String nome = "Raniel";
+
+    //Tipo final(constante, ou seja, o valor não pode ser alterado)
+    final double VALOR_DE_PI = 3.14;
+
 }
