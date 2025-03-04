2552536a4188981e154c00fa7ed0a88f0cd18cb6
raniel silva <ranielcorreia1997@gmail.com>
2025-03-03 22:40:36 -0300

    aula terminal e argumentos

---
+++ src/edu/raniel/javadoc/Calculadora.java
@@ -0,0 +1,26 @@
+package edu.raniel.javadoc;
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operações matemáticas entre números inteiros.
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classe
+ * para desfrutar dos recursos oferecidos pelo autor.
+ * </p>
+ *
+ * @author Raniel Silva
+ * @version 1.0
+ * @since 03/03/2025
+ */
+public class Calculadora {
+
+    /**
+     * Soma dois números inteiros.
+     *
+     * @param numeroUm   o primeiro número inteiro.
+     * @param numeroDois o segundo número inteiro.
+     * @return o resultado da soma dos dois números.
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
---
+++ src/edu/raniel/terminal_e_argumentos/MinhaClasse.java
@@ -0,0 +1,7 @@
+package edu.raniel.terminal_e_argumentos;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Executando pelo terminal");
+    }
+}
